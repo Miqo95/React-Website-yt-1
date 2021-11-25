7f1ed35047b9278f02f704f7944bdfa88f4fd18a
Miqo95 <gmiqael@bk.ru>
2021-11-25 16:56:30 +0400

    day-3Work

--- my-app/src/components/HeroSection.js
+++ my-app/src/components/HeroSection.js
@@ -2,11 +2,15 @@ import React from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
+import video from "../videos/video-2.mp4";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="./videos/video-2.mp4" autoPlay loop muted />
+      {/* <video src="../videos/video-1.mp4" autoPlay loop muted /> */}
+      <video autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
@@ -21,6 +25,7 @@ function HeroSection() {
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
           WATCH TRAILER <i className="far fa-play-circle" />
         </Button>
